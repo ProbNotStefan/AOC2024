e768142e82dfab8350e676d3cc851eeea38b65a8
Stefan <159160498+ProbNotStefan@users.noreply.github.com>
2024-12-03 12:44:11 +0000

    e

--- 2-1.py
+++ 2-1.py
@@ -1,7 +1,5 @@
 c,b,d = 0, True, False
 for line in open("2.txt","r+"):
     line=list(map(int,line.split()));d,b = line[0]-line[1] < 0, True
-    for i in range(len(line)-1):
-        if abs(line[i]-line[i+1]) > 3 or abs(line[i]-line[i+1]) == 0 or (d == True and line[i]-line[i+1] > 0) or (d == False and line[i]-line[i+1] < 0): b = False
-    if b == True: c += 1
+    if False not in [(abs(line[i]-line[i+1]) > 3 or abs(line[i]-line[i+1]) == 0 or (d == True and line[i]-line[i+1] > 0) or (d == False and line[i]-line[i+1] < 0)) for i in range(len(line))]: c += 1
 print(c)
