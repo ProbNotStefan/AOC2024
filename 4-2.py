797a9927363eab2386a8ee14d11144bc27268e74
ProbNotStefan <djumaste@hotmail.com>
2024-12-04 18:27:35 +0000

    4-2 1liner

--- 4-2.py
+++ 4-2.py
@@ -1,5 +1,1 @@
-c,l = 0,list(open("4.txt", "r+"))
-for i in range(1, len(l) - 1):
-    for j in range(1, len(l[0]) - 1): 
-        c += int(l[i][j] == "A" and (((l[i-1][j-1] == "M" and l[i+1][j+1] == "S") or (l[i-1][j-1] == "S" and l[i+1][j+1] == "M")) and ((l[i+1][j-1] == "M" and l[i-1][j+1] == "S") or (l[i+1][j-1] == "S" and l[i-1][j+1] == "M"))))
-print(c)
+l=list(open("4.txt", "r"));print(sum([int(l[i][j] == "A" and (((l[i-1][j-1] == "M" and l[i+1][j+1] == "S") or (l[i-1][j-1] == "S" and l[i+1][j+1] == "M")) and ((l[i+1][j-1] == "M" and l[i-1][j+1] == "S") or (l[i+1][j-1] == "S" and l[i-1][j+1] == "M")))) for j in range(1, len(l[0]) - 1) for i in range(1, len(l) - 1)]))
